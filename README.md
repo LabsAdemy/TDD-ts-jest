6962230b77708d1ce4555176e08ad35b7db70ac8
Alberto Basalo <albertobasalo@hotmail.com>
2021-11-29 10:52:56 +0100

    chore: ts jest config

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TDD-ts-jest
+
 Test driven development with Jest and Typescript
+
+```terminal
+npm i -D typescript jest
+npm i -D @types/node @types/jest
+npm i -D ts-jest
+npx ts-jest config:init
+npx tsc --init
+```
---
+++ src/0-hello-world/hello-world.test.ts
@@ -0,0 +1,9 @@
+import { getHelloWorld } from "./hello-world";
+
+describe("the getHelloWorld function", () => {
+  it('should return "Hello World!"', () => {
+    const actual = getHelloWorld();
+    const expected = "Hello World!";
+    expect(actual).toBe(expected);
+  });
+});
