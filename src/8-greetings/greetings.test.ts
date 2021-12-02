8c74e4efee705bc451c2ef72020ff7526bb2c9db
Alberto Basalo <albertobasalo@hotmail.com>
2021-12-02 16:47:02 +0100

    g 1

--- src/8-greetings/greetings.test.ts
+++ src/8-greetings/greetings.test.ts
@@ -1,5 +1,7 @@
 import { greetings } from './greetings';
 
+// 1️⃣ Write a method greetings(name) that interpolates name in a simple greeting.
+
 describe('', () => {
   it('', () => {
     expect(greetings()).toBe('Hello World');
