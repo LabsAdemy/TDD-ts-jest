adf4c1cc2de4f552cb12ffb31cb7982fcaed067d
Alberto Basalo <albertobasalo@hotmail.com>
2021-12-02 16:46:12 +0100

    greetings

---
+++ src/8-greetings/greetings.test.ts
@@ -0,0 +1,7 @@
+import { greetings } from './greetings';
+
+describe('', () => {
+  it('', () => {
+    expect(greetings()).toBe('Hello World');
+  });
+});
---
+++ src/8-greetings/greetings.ts
@@ -0,0 +1,3 @@
+export function greetings(): any {
+  return 'Hello World';
+}
